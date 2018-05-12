55183f44d7042e04865e86f3b0ee0ab9fb105346
romano <romano@alunos.utfpr.edu.br>
2018-05-12 14:53:16 -0300

    added ex1

---
+++ ex1.py
@@ -0,0 +1,7 @@
+var_int = 2
+var_float = 3.0
+
+print("Soma: ", var_int + var_float)
+print("Subtração: ", var_int - var_float)
+print("Divisão: ", var_int / var_float)
+print("Multiplicação: ", var_int * var_float)
