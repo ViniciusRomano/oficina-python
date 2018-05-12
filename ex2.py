1d887e8d97941838b9822ae96831c9488168b3c1
romano <romano@alunos.utfpr.edu.br>
2018-05-12 15:26:33 -0300

    ex2

---
+++ ex2.py
@@ -0,0 +1,7 @@
+nome_completo = "Vinicius Silva"
+print("Quarta letra:", nome_completo[3])
+print("Ultima letra:", nome_completo[-1])
+print("Três primeiras letras:", nome_completo[:3])
+print("Duas ultimas letras:", nome_completo[-2:])
+print("Lista: ", nome_completo.split(" "))
+print("Quantidade de letras:", len(nome_completo.replace(" ", "")))
